416a633edd26b628ade85b41b916f4f5eca81e31
efe overare <elfredaigelige@gmail.com>
2022-06-23 22:45:00 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let x = parseFloat(parseInt(prompt("enter first number")));
+let y = parseFloat(parseInt(prompt("enter second number")));
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+alert("the sum of the given numbers is:"+z);
+alert("the difference of the given numbers is:" +a);
+alert("the product of the given numbers is:" +b);
+alert("the quotient of the given numbers is:" +c);
